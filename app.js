e660a866b70ce7a8643c99b05d814f032a4528de
ashleyshakir <ashleymshakir@gmail.com>
2023-08-30 11:06:14 -0400

    updated project card css

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ const navList = document.getElementById('nav-list')
 function toggleButton(){
     navList.classList.toggle('show')
 }
-
 hamburgerButton.addEventListener('click', toggleButton)
+const aboutMeLink = document.querySelector(".button a")
+const aboutMeButton = document.querySelector(".button")
+aboutMeButton.addEventListener("mouseenter",function(){
+    aboutMeLink.style.color = "#f2ebe9"
+})
+aboutMeButton.addEventListener("mouseleave",function(){
+    aboutMeLink.style.color = "#de5499"
+})
