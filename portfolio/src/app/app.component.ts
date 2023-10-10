4bb68577d62a1b38d4035f3a73fa627baaa1ab1d
ashleyshakir <ashleymshakir@gmail.com>
2023-10-10 18:09:15 -0400

    home page completed

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // AOS initialization - disabled on screens with a width less than or equal to 840 pixels
+    AOS.init({
+      disable: function () {
+        let maxWidth = 840;
+        return window.innerWidth <= maxWidth;
+      },
+    });
+  }
 }
